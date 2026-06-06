382db4fe0409f77bd3d9c606ca4898bd999aa3ba
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-06 22:30:18 +0530

    Add text code about Quotes and print() function

---
+++ Text.py
@@ -0,0 +1,10 @@
+# We can use print()function as many times as you want
+# Each call prints text on a new line by default:
+print("Hello World")
+print("I am learning Python.")
+print("It is awesome!")
+#text in Python must be inside quotes. We can use either " " double quotes or ' ' single quotes:
+print("This will work!")
+print('This will also work!')
+# Quotes are mandatory for text if we don't put the text inside the quotes it will throw an error.
+
