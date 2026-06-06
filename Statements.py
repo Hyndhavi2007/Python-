cc9d1142c166bca2ca57bfe3b5f79d511d22dfe1
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-06 22:03:39 +0530

    Add Statements python code

---
+++ Statements.py
@@ -0,0 +1,8 @@
+# A computer program is a list of "instruction" to be "executed" by a computer
+# These programming instructions are called statements
+print("hello World!!")
+print("Have a good day.")
+print("Learning python is Fun!")
+# Semicolon is optional in Python, We can write myltiple statement on one line by separating them with ;
+# But thiis is rarely used because it makes hard to read
+print("Hello");print("How are you?");print("Bye bye!")
