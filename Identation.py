7054fcbbae25ae21a4d43ba9184577f96e7ba2df
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-06 21:46:18 +0530

    Add identation code

---
+++ Identation.py
@@ -0,0 +1,6 @@
+# Identation refers to to the spaces at the beginning of a code line.
+# Identation is very important in Python.It is used to indicate Block of code.
+if 5 > 2:
+    print("Five is graeter than two!")
+    if 10 > 5:
+        print("Ten is greater than Five")
