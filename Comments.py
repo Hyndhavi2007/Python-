da6da5a91c4292f4a7792df060645b44f1d68497
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-06 22:57:43 +0530

    Add Cooments python code explaining about comments and multiline comments

---
+++ Comments.py
@@ -0,0 +1,12 @@
+# Comments can be used to explain python code
+# Comments can be used to amke the code more readable.
+# Comments can be used to prevent execution while testing code.
+# Comments can be placed at the end of line
+# Multiline Comments
+# python does not have a syntax for multiline commments, to add a multiline comment we could insert # in each line
+"""
+This is a comment
+written in 
+more than just one line
+"""
+print("Hello World")
