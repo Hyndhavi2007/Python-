0cfb2f0373f37440aec17085e0c242bf9b5504e6
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-06 22:48:41 +0530

    Add mathematical operation python code

---
+++ Math.py
@@ -0,0 +1,12 @@
+# We can do math inside the print() function:
+print("Addition")
+print(3 + 3)
+print("Subtraction")
+print(10 - 5)
+print("Multiplication")
+print(3 * 3)
+print("Division")
+print(10 /3)
+print("Division: Remainder")
+print(10 % 3)
+
