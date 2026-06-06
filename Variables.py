6511b34b932fd5512fdc44f9f7937365fe1b5394
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-06 23:11:28 +0530

    Add variables python code

---
+++ Variables.py
@@ -0,0 +1,10 @@
+# Variables are containers for storing data values
+# Python has no command for declaring a variable.
+# A variable is created the moment when we first assign a value to it.
+x = 5
+y = "John"
+print(x)
+print(y)
+# variables do not need to be declared with any particular type, and can even change type after they have been set.
+x = "Sally"
+print(x)
