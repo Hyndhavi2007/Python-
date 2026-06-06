bce2de681e818ef2564f0aa0182e39e322debc80
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-06 22:35:02 +0530

    Add print Without new line using end parameter

---
+++ printWithoutNewLine.py
@@ -0,0 +1,4 @@
+# By default, the print() function ends with a new line.
+# If we want to print multiple words on the same line, we can use the end parameter
+print("hello World!", end = " ")
+print("I will print on the same line.")
