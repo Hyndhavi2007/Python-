e2a1311c29268261c394977ec10762a970e6ad22
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-07 17:59:34 +0530

    Add global keyword python code

---
+++ globalkeyword.py
@@ -0,0 +1,11 @@
+# As we know that when we create a variable inside a function ,
+# the variable is local variable, and can only be used inside a function
+"""
+To create a  global variable inside a function ,
+we use the "global" keyword.
+"""
+def myfunc():
+    global x 
+    x = "fantastic"
+myfunc()
+#print("Python is " + x)
