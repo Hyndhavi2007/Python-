3abb7423db91ecc8e5bbeabd6b0482615cbdd091
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-07 16:22:44 +0530

    Add Muti words variables names

---
+++ MultiWordsVariableNames.py
@@ -0,0 +1,13 @@
+# Variable names with more than one word can be difficult to read
+# There are several techniques we can use to make them more readable.
+"""
+Camel case -  Each word, except the first, starts with a capital letter:
+Pascal Case - Each word starts with a Capital letter:
+Snake Case - Each case is separated by an underscore:
+"""
+myVariablename = "Max"
+MyVariableName = "Lewis"
+my_variable_name = "George"
+print(myVariablename)
+print(MyVariableName)
+print(my_variable_name)
