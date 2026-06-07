ebbc150fc50b4503ca3f4cdc26a6ec5f72e60cc1
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-07 16:35:27 +0530

    Add Assigning multiple values to variables python code

---
+++ AssigningMultipleValuesToVariables.py
@@ -0,0 +1,13 @@
+#Python allows you to assign values to multiple variables in one line:
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+# We should make sure that the number of variables matches the number of values, or else we will get an error.
+
+# One value to Multiple Variables
+# We can assign the same value to mutliple variables in one line:
+a = b = c = "Apple"
+print(a)
+print(b)
+print(c)
