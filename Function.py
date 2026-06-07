85cc51b1ce1775e8664def56662d1656643fbf89
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-07 17:48:09 +0530

    Add Function python code

---
+++ Function.py
@@ -0,0 +1,11 @@
+# If we create a variable inside a finction it is called gloabal variable.
+# If we create a variable with the same name inside a function , this variable will be local , 
+# and can only be used insiode the function.
+# The global variable with the same name will remain as it was, global and with the original value.
+x = "awesome"
+def myfunc():
+    x = "fantastic"
+    print("Python is " + x)
+myfunc()
+
+print("Python is " + x)
