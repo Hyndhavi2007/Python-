d70f477e366a0211ce974d8ae6be7eff4ac311d0
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-07 17:03:44 +0530

    Add print() python code

---
+++ Print().py
@@ -0,0 +1,7 @@
+# The print() function is often used to output variables.
+# print() function, we can output multiple variables, separated by a comma.
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+print(  x  +  y  +  z )
