e833fb21e59e14c8ab284414ab8a49a47fda1be8
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-07 15:48:54 +0530

    Add cating python code

---
+++ Casting.py
@@ -0,0 +1,10 @@
+#If we want to specify the data type of variable, this can be done with casting.
+x = str(3)
+y = int(3)
+z = float(3)
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
