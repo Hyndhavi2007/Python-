2f3f5da29f5f1bbe5f26e773c4cbba9dbe2c8391
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-07 16:09:01 +0530

    Ad variable names python code explaining about the rules of  naming a variable and the important thing is variable names are case sensitive

---
+++ Variablenames.py
@@ -0,0 +1,21 @@
+#A variable can have a short name (like x and y) or a more descriptive name(age, name, number)
+"""
+Rules for python variables
+1. A variable name must start with letter or underscore character.
+2. A variable cannot start with a number
+3. A variable name can only contain alpha - numeric characters and underscores (A -z, 0-9 and _).
+4. Variable names are case- sensitive (age, Age, AGE).
+5. A variable cannot be any of the Python keywords.
+"""
+myvar = "John"
+_myvar = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+print(myvar)
+print(_myvar)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
