3e36b2031ad88d59d7c95ac111e02b513e6cf3a0
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-08 21:12:35 +0530

    Add Whitespace

---
+++ Whitespace.py
@@ -0,0 +1,7 @@
+# Whitespace is the space before and/or after the actual text,and very often we wnat to remove this space.
+"""
+The strip() method removes any whitespace from beginning or the end:
+"""
+a = " Hello, World! "
+print(a)
+print(a.strip())
