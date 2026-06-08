2c9da2af58626316d1e55750d279915f7d4481db
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-08 21:31:13 +0530

    Add SplitString

---
+++ SplitString.py
@@ -0,0 +1,6 @@
+"""
+The split() method return a list where the text between the specified  separator
+The split() method splits the string into substrings if it finds instances of the separator:
+"""
+a = "Hello, World!"
+print(a.split(","))
