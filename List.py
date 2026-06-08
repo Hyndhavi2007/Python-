b864a4b6128dce82da85fc56996a5175ca9792da
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-08 22:21:32 +0530

    Add List

---
+++ List.py
@@ -0,0 +1,23 @@
+"""
+Lists are used to stroe multiple items in a single variable.
+Lists are  are one of 4 built-in data types in Python used to store collection of data,
+The other 3 are Tuple, Set, and Dictataion, all with different qualities and usage.
+Lists are created using square brackets:
+"""
+List = ["Apple","Banana", "Cherry"]
+print(List)
+"""
+List items are ordered, changable, and allow duplicate values(Mutable).
+List items are indexed, the first item has index [0], the second item has index[1] etc.
+When we say list items are ordered, it means that the list items have a defined order, and that order will not change.
+if we add new items to list, the new items will be placed at the end of the list.
+List items can be of any data type.
+A list can contain diiferent data types.
+"""
+List1 = ["abc", 34, True, 40, False, "APPLE", "CHERRY"]
+print(List1)
+print(type(List))
+"""
+list() constructor
+It is also
+"""
