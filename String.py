91d86198be08a70b3de3f296f7c2164ef0fde90a
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-08 20:22:46 +0530

    Add Strings python code

---
+++ String.py
@@ -0,0 +1,20 @@
+# Strings in python are surrounded by either single quotation marks, or double quoatation marks.
+#'hello' is same as "hello"
+# String is defined as immutable sequence of unicode chararcters used to represent textual data.
+print('hello')
+print("hello")
+"""
+We can use quotes inside a string, as long as they don't match the quotes surrounding the string
+"""
+print("It's alright")
+print("He is called'Johnny'")
+print('He is called "Johnny"')
+# Assigning a string to a variable is done with the variable name followed by an equal sign and the string:
+a = "Hello"
+print(a)
+# Multiline String: we can assign a multiline string to a variable using three quotes:
+b = """Lorem ipsum dolor sit amet,
+consecteur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(b)
