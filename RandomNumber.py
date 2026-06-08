297e3311d673fdfba9932e2db03a2aa8d8e5c91a
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-08 17:29:27 +0530

    Add random module in python

---
+++ RandomNumber.py
@@ -0,0 +1,6 @@
+"""
+python does not hava a random() function to make a rabdom number, but python has a 
+built in module called random that can be used to make random numbers:
+"""
+import random
+print(random.randrange(1,10))
