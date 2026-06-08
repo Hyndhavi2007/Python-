7cb3f8cbadf256a7dc4cef51fe19782dea37e557
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-08 21:08:00 +0530

    Add Modify string using lower( and upper( methods

---
+++ ModifyStrings.py
@@ -0,0 +1,9 @@
+# Python has a set build-in methods that can be used on strings
+"""
+Upper case: the upper() method returns the string in upper case.
+Lower case: the lower() method returns the string in lower case.
+"""
+a = "hello world!"
+print(a.upper())
+b = "HELLO WORLD!"
+print(b.lower())
