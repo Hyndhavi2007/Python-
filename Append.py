5dd6239100130c95aa055368ce6c88d6e2acf83b
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-09 21:18:58 +0530

    Add append method in list

---
+++ Append.py
@@ -0,0 +1,10 @@
+# Append : To add an item  to the end of the list, use the append() method:
+fruits = ["apple","banana","cherry"]
+print(fruits)
+fruits.append("Durain")
+print(fruits)
+"""
+append is used to add an item in the end of the list ,
+whereas insert is used to insert an item at a specified index.
+"""
+
