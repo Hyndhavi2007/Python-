51ee06c8b7d6a57a9c97cee7afb5ca7f29f409ef
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-09 21:05:02 +0530

    add Insert

---
+++ Insert.py
@@ -0,0 +1,9 @@
+"""
+To insert a new list item, without replacing any of the existing values,
+we can use the insert() method
+The insert() method inserts an item at the specified index:
+"""
+veggies = ["Onion","Tomato","Carrot","Beetroot"]
+print(veggies)
+veggies.insert(2, "Ginger")
+print(veggies)
