d6632972d04839c6b45b235ab60c13feb3ca581d
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-10 20:07:20 +0530

    Add Format String

---
+++ FormatString.py
@@ -0,0 +1,15 @@
+name = "Max"
+age = 28
+score = 99.8798
+# basic
+print(f"Name: {name}, Age: {age}")
+# Math inside
+print(f"Age next year: {age +1}")
+# Formatong Numbers
+print(f"Score: {score:.2f}")
+print(f"Score: {score:.0f}")
+print(f"Salaray: {100000000:,}")
+# Padding/ alignment
+print(f"{'left': <10}|")
+print(f"{'right':>10}|")
+print(f"{'centre:^10'}|")
