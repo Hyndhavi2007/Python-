58b4753ba34250b25a1267e03e028d60ac32952c
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-11 16:05:40 +0530

    Add conditionals

---
+++ Conditionals.py
@@ -0,0 +1,17 @@
+marks = 85
+if marks >= 90:
+    print("Grade A - Excellent!")
+elif marks >= 80:
+    print("Grade B - Good")
+elif marks >= 70:
+    print("Grade C - Average")
+elif marks >= 60:
+    print("Grade D - Below Average")
+else:
+    print("Grade F - Fail")
+"""
+if is mandatory — always first
+elif is optional — can have as many as you want
+else is optional — catches everything not caught above
+Once one block runs, the rest are skipped
+"""
