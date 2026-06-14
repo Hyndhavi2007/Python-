9941b2db0306b678a687e5dbfa71762797880199
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-14 21:34:54 +0530

    Add nested dictionary

---
+++ Nested_Dictionary.py
@@ -0,0 +1,11 @@
+students = {
+    "s001": {"name": "Rahul", "marks": 85,"city": "Hyderabad" },
+    "s002": {"name": "Priya", "marks": 92,"city": "Mumbai"} ,
+    "s003": {"name": "Arun", "marks": 78, "city": "Chennai"}
+}
+# Access nested value
+print(students["s002"]["name"])
+print(students["s001"]["marks"])
+# Loop through nested dict
+for sid, info in students.items():
+    print(f"{sid}: {info['name']} - {info['marks']}")
