2d141aaf467fdeb39fcf6e3554c66f065713999f
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-15 16:02:58 +0530

    Add Use of tuples

---
+++ Use_Of_Tuples.py
@@ -0,0 +1,16 @@
+def get_dimensions():
+    return 1920, 1080
+width, height = get_dimensions()
+print(width, height)
+a,b,c = (10,20,30)
+first, *rest = (1,2,3,4,5)
+print(first)
+print(rest)
+x,y = 5,10
+x,y = y,x
+print(x,y)
+locations = {
+    (28.6, 77.2): "Delhi",
+    (19.0,72.8) : "Mumbai"
+}
+# returns multiple value from functions
