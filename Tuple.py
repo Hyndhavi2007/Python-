d141f0a15f0da0835f0be0d9c51189924333ab02
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-15 15:49:26 +0530

    Add tuple

---
+++ Tuple.py
@@ -0,0 +1,12 @@
+# a tuple is like a list but immutable - you can't change it after creation.
+point = (10,20)
+rgb = (255,128,0)
+single = (42,)
+empty = ()
+# Without parantheses also works
+coords = 3,5
+print(point[0])
+print(point[1])
+print(rgb[-1])
+print(rgb[1:])
+print(single[0])
