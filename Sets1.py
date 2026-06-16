2b516d82a0132fcbcb911b191cde5445fd47ba60
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-16 20:42:09 +0530

    Add a problem using set concept

---
+++ Sets1.py
@@ -0,0 +1,12 @@
+set = {1,2,3,4,5}
+set.add(6)
+print(set)
+set.remove(1)
+print(set)
+length = len(set)
+print(length)
+if 4 in set:
+    print("4 is present in the set")
+else:
+    print("4 is not present in the set")
+print(set)
