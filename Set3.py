23c5e8ab7eca73dbe0f5d3c911de16de454292f1
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-16 21:31:37 +0530

    Add Set problem using some set operations

---
+++ Set3.py
@@ -0,0 +1,11 @@
+numbers = {1,1,2,3,4,5,6,6,7,7,8,9,9,9,9,9,0,0,10}
+unique_numbers = list(set(numbers))
+print(set)
+print("Set after removing the duplicates: ", unique_numbers)
+sentence = "Python is easy and powerful"
+words = sentence.split()
+print(words)
+unique_words = set(words)
+print("The unique words from the sentence are ", unique_words)
+count = len(unique_words)
+print("The length of the unique words is ", count)
