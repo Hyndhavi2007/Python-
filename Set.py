abc92b9d125be6bb8802a9d730e7b6eaec31ecb1
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-06-16 20:13:44 +0530

    Add Set

---
+++ Set.py
@@ -0,0 +1,6 @@
+# A set is an unorderd collection of unique values -
+# duplicates are automatically removed.
+s = {1,2,3,4}
+from_list = set([1,2,2,3,3,3])
+print(from_list)
+empty = set() # {} creates empty DICT, not set!
